439fc83dff35135910fc303c8f421e85bc0d278a
amansingh202 <hellraisertelevision@gmaill.com>
2023-12-16 23:07:48 -0600

    This is a new commit for Debt ratio.java

---
+++ DebtRatio.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class DebtRatio {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double income=in.nextDouble();
+		double otherMonthlyDebt=in.nextDouble();
+		// add your code here
+
+		System.out.println( "Lower Limit: $" + lowLimit);  //formatted output not required
+		System.out.println( "High Limit: $" + highLimit);  //formatted output not required
+	}
+}
