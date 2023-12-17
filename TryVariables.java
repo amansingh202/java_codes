b1b39e0cb45fd7b7c566f06a34dcb63052ce2965
amansingh202 <60036234+amansingh202@users.noreply.github.com>
2023-12-16 23:01:38 -0600

    Add files via upload
    Try variables

---
+++ TryVariables.java
@@ -0,0 +1,23 @@
+public class TryVariables {
+	
+	// This example shows how to declare and initialize variables
+	public static void main( String [] args ) {
+		int testGrade = 100;
+		long cityPopulation = 425612340;
+		byte ageInYears = 19;
+		float  salesTax = .05F;		// the trailing F makes a float numeric literal   
+		double interestRate = 0.725
+		double avogadroNumber = 602214076000000000000000.;
+		char finalGrade = 'A';
+		boolean isEmpty = true;
+
+		System.out.println( "testGrade is " + testGrade );
+		System.out.println( "cityPopulation is " + cityPopulation );
+		System.out.println( "ageInYears is " + ageInYears );
+		System.out.println( "salesTax is " + salesTax );
+		System.out,println( "interestRate is " + interestRate );
+		System.out.println( "avogadroNumber is " + avogadroNumber );
+		System.out.println( "finalGrade is " + finalGrade );
+		System.out.println( "isEmpty is " + isEmpty );
+	}
+}
