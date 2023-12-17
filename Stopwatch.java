6c568160e056ff590801c21356b0fd7d5212004a
amansingh202 <hellraisertelevision@gmaill.com>
2023-12-16 23:23:19 -0600

    Stop watch code

---
+++ Stopwatch.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Stopwatch {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int seconds=in.nextInt();
+		// add your code here
+
+		System.out.println( seconds+" seconds equals " + hours + ":" + minutes + ":" + leftOver);
+	}
+}
