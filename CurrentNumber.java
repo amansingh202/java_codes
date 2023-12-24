5682bb9de8ce745d8a5a5ab579374df798ab0fb6
amansingh202 <hellraisertelevision@gmail.com>
2023-12-23 22:43:21 -0500

    current number java code

---
+++ CurrentNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CurrentNumber {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int currentNumber = in.nextInt();
+    // add your code here    
+
+ 
+ 
+ 
+ 
+    System.out.println(currentNumber);
+  }
+}
