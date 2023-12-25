d8919bcc16ccf2c843dd7bb9b30d38303ebfac4d
amansingh202 <hellraisertelevision@gmail.com>
2023-12-25 00:02:55 -0500

    Is candidate java code Lab 04

---
+++ IsCandidate.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class IsCandidate {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int satScore=in.nextInt();
+    double gpa=in.nextDouble();
+    int age=in.nextInt();
+    boolean isCandidate;
+    // add your code here
+
+
+
+    System.out.println(isCandidate);
+  }
+}
