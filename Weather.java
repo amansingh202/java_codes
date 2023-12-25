cdd3df0f844c28a692f490554d18f54617babb9f
amansingh202 <hellraisertelevision@gmail.com>
2023-12-24 23:58:20 -0500

    Weather java code for Lab04

---
+++ Weather.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Weather {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int temperature = in.nextInt();    
+    // add your code here    
+
+
+
+  }
+}
