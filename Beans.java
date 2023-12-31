d02c5e68b83093bc8b61538f3573ed656c106131
amansingh202 <hellraisertelevision@gmail.com>
2023-12-30 23:07:11 -0500

    Lab 5 java code Beans

---
+++ Beans.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Beans {
+	public static void main(String[] args) {
+  // include your code here
+
+}
+	public static int computerMove(int pileBeans, int computerBeans) {
+  // include your code here
+
+	}
+}
