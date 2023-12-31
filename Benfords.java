fb1186db44e9b7203f8c4b93c644824c81f1ce90
amansingh202 <hellraisertelevision@gmail.com>
2023-12-30 23:07:46 -0500

    Lab 05 java code for Benfords Java

---
+++ Benfords.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;  
+import java.math.BigInteger; 
+import java.text.NumberFormat;
+public class Benfords {
+	public static void main(String[] args) {
+		NumberFormat pctFormat = NumberFormat.getPercentInstance();
+
+	}
+
+	public static BigInteger factorial(int n) {
+
+	}
+
+	public static BigInteger fib(int n) {
+
+	}
+	
+	public static int leadingDigit(BigInteger n) {
+	  
+	}
+
+}
