4b3df3f9a6282db48d26a4c86511566a93d2f7b9
amansingh202 <hellraisertelevision@gmail.com>
2024-01-04 16:27:35 -0600

    Lab 05 java code for Switch no switch

---
+++ SwitchNoSwitch.java
@@ -0,0 +1,14 @@
+import java.util.*;   
+public class SwitchNoSwitch {
+	public static Random myRandomGenerator = new Random(123456789);
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int trials = in.nextInt();
+
+	}
+	
+	public static int montyHallTrial (boolean playerSwitch) {
+
+
+	}
+}
