6a3fcae942dc805734479f40409341b5e7586811
amansingh202 <hellraisertelevision@gmail.com>
2024-01-21 22:32:05 -0600

    alb 06 part2 java code for Date in Java

--- Date.java
+++ Date.java
@@ -1,15 +1,13 @@
 public class Date {
-  private int month;
+  private i nt month;
   private int day;
   private int year;
   
-  private static final int [] validDays = {0,31,28,31,30,31,30,31,31,30,31,30,31};  
+  private static final int [] validDays = {0,31,29,31,30,31,30,31,31,30,31,30,31};  
   private static final int DEFAULT_DAY=1, DEFAULT_MONTH=1, DEFAULT_YEAR=2000;
 
   public Date( )  {
-	  month=DEFAULT_MONTH;
-	  day=DEFAULT_DAY;
-	  year=DEFAULT_YEAR;
+    setDate(DEFAULT_MONTH, DEFAULT_DAY, DEFAULT_YEAR);
   }
 
   public Date( int mm, int dd, int yyyy ) {
@@ -25,10 +23,10 @@ public class Date {
   }
 
   private void setDay( int dd ) {
-  	int maxDays=validDays[month];
-	  if (leapYear() && month==2) {
-		  maxDays=29;
-	  }
+	int maxDays=validDays[month];
+	if (leapYear() && month==2) {
+		maxDays=28;
+	}
     day = ( dd >= 1 && dd <= maxDays ? dd : DEFAULT_DAY );
   }
 
@@ -47,60 +45,18 @@ public class Date {
   }
 
   public boolean equals( Date d ) {
-    return ( month == d.month && day == d.day  && year == d.year );
+    if ( month == d.month
+         && day == d.day
+         && year == d.year )
+      return true;
+    else
+      return false;
   }
 
   public boolean leapYear() {
-    return ((year%4==0) && ((year%100!=0)||(year%400==0))); 
-  }
-  
-  public int compareTo(Date that) {    // improvement would to return the number of days between, like String returns the difference in ASCII values
-	  if (this.year<that.year) {
-	    return -1;
-	  }
-  	else {
-  	  if (this.year>that.year) {
-  	     return 1;
-  	  }
-      else {
-        if (this.month<that.month) {
-          return -1;
-        }
-    		else {
-    		  if (this.month>that.month) {
-    		    return 1;
-    		  }
-      		else {
-		        if (this.day<that.day) {
-		          return -1;
-		        }
-      			else {
-      			  if (this.day>that.day) {
-      			    return 1;
-      			  }
-        			else {
-        			  return 0;
-        			}
-      			}
-      		}
-    		}
-      }
-		}
-  }
-  public void incrementMonth() {
-	  if (month==12) {
-		  setMonth(1);
-		  setYear(year+1);
-    }
-    else {
-      setMonth(month+1);
-    }
-    int maxDays = validDays[month];    
-	  if (leapYear() && month==2) {
-	    maxDays=29;
-	  }
-	  if (day>maxDays) {
-	    setDay(maxDays);
-	  }
+	if (!((year%4==0) && 
+		((year%100!=0)||(year%400==0))))
+		return false;
+	else return true;
   }
 }
